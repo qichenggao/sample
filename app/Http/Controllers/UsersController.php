c7662703db0da36c3d1320fdb0cba1488ed0ed0b
Qcg <qichenggao@126.com>
2018-02-28 02:39:20 +0000

    Add user statuses

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -31,9 +31,11 @@ class UsersController extends Controller
         return view('users.create');
     }
 
-    public function show(User $user){
-        $this->authorize('update', $user);
-        return view('users.show',compact('user'));
+    public function show(User $user) {
+        $statuses = $user->statuses()
+                         ->orderBy('created_at','desc')
+                         ->paginate(30);
+        return view('users.show',compact('user','statuses'));
     }
 
     public function store (Request $request) {
@@ -113,5 +115,7 @@ class UsersController extends Controller
         session()->flash('success', '恭喜你，激活成功！');
         return redirect()->route('users.show', [$user]);
     }
+
+
 
 }
